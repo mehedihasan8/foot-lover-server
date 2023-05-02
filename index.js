3f954a9df16a13489e6b03acdf83f48162271ddc
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2023-05-02 15:36:20 +0600

    complit data sent to websit

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 const express = require("express");
-const app = express;
+const cors = require("cors");
+const app = express();
+const port = 5000;
+
+const chef = require("./data/chef.json");
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(chef);
+});
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const SelectedChef = chef.find((c) => c.id === id);
+  res.send(SelectedChef);
+});
+
+// app.get("/", (req, res) => {
+//   res.send("Food Lovers Server runnig");
+// });
+
+app.listen(port, () => {
+  console.log(`Food Lovers Server runnig ${port}`);
+});
